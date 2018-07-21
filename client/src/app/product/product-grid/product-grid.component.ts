bfe0924458c8eaa773f70583455351138bf0d2e2
zlkca <likzhang@gmail.com>
2018-07-21 09:19:15 -0400

    Change product list to responsive

--- client/src/app/product/product-grid/product-grid.component.ts
+++ client/src/app/product/product-grid/product-grid.component.ts
@@ -12,22 +12,22 @@ import { CartActions } from '../../order/order.actions';
 const ADD_IMAGE = 'add_photo.png';
 
 @Component({
-	providers:[CommerceService],
-  selector: 'product-grid',
-  templateUrl: './product-grid.component.html',
-  styleUrls: ['./product-grid.component.scss']
+    providers: [CommerceService],
+    selector: 'product-grid',
+    templateUrl: './product-grid.component.html',
+    styleUrls: ['./product-grid.component.scss']
 })
 export class ProductGridComponent implements OnInit {
-	productList:Product[] = [];
-    MEDIA_URL:string = environment.MEDIA_URL;
+    productList: Product[] = [];
+    MEDIA_URL: string = environment.MEDIA_URL;
     placeholder = environment.MEDIA_URL + ADD_IMAGE;
-    subscription:any;
-    cart:any;
+    subscription: any;
+    cart: any;
 
-    @Input() data: Product[];
+    @Input() products: Product[];
     @Input() mode: string;
 
-    ngOnInit(){
+    ngOnInit() {
         // let self = this;
         // this.commerceServ.getProductList().subscribe(
         //     (r:Product[]) => {
@@ -38,39 +38,34 @@ export class ProductGridComponent implements OnInit {
         //     });
     }
 
-    constructor(private commerceServ:CommerceService,
-        private router:Router,
-				private modalService: NgbModal,
-        private ngRedux:NgRedux<IAppState>//,
+    constructor(private commerceServ: CommerceService,
+        private router: Router,
+        private modalService: NgbModal,
+        private ngRedux: NgRedux<IAppState>//,
         // private actions: CartActions
-        ) {
+    ) {
 
         // this.subscription = ngRedux.select<ICart>('cart').subscribe(
         //   cart=> this.cart = cart);
     }
 
-    onClick(p){
-        if(this.mode == 'edit'){
 
-        }else{
-					// const modalRef = this.modalService.open(ProductComponent);
-          this.router.navigate(["product/"+p.id]);
-        }
+    addToCart(p) {
+        this.ngRedux.dispatch({
+            type: CartActions.ADD_TO_CART, payload:
+                { pid: p.id, name: p.name, price: p.price, restaurant_id: p.restaurantId }
+        });
     }
 
-    addToCart(p){
-      this.ngRedux.dispatch({type:CartActions.ADD_TO_CART, payload:
-          {pid:p.id, name:p.name, price:p.price, restaurant_id:p.restaurantId}});
+    removeFromCart(p) {
+        this.ngRedux.dispatch({ type: CartActions.REMOVE_FROM_CART, 
+            payload: { pid: p.id, name: p.name, price: p.price, restaurant_id: p.restaurantId } });
     }
 
-    removeFromCart(p){
-      this.ngRedux.dispatch({type:CartActions.REMOVE_FROM_CART, payload:{pid:p.id, name:p.name, price:p.price, restaurant_id:p.restaurantId}});
-    }
-
-    getImageSrc(p){
-        if(p.fpath){
+    getImageSrc(p) {
+        if (p.fpath) {
             return this.MEDIA_URL + p.fpath;
-        }else{
+        } else {
             return this.MEDIA_URL + ADD_IMAGE;
         }
     }
