f7729176ce953604dcf4196c9f6d26a942639e7f
zlkca <likzhang@gmail.com>
2018-09-13 08:03:49 -0400

    Add weixin auth

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,10 +1,29 @@
 'use strict';
 
 var path = require('path');
+var crypto = require('crypto');
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   // router.get('/', server.loopback.status());
+
+  router.get('/wx', (req, res)=>{
+    let token = 'testToken20';
+    let timestamp = req.query.timestamp;
+    let nonce = req.query.nonce;
+    let signature = req.query.signature;
+    let list = [token, timestamp, nonce].sort();
+    let sha1 = crypto.createHash('sha1');
+    let s = list.join('');
+    let hash = sha1.update(s).digest('hex');
+    console.log(hash);
+    if (hash === signature) {
+      res.send(req.echostr);
+    } else {
+      res.send('');
+    }
+  });
+
   server.use(router);
 };
