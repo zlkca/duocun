8144f876279ed1e82cb10e94617605e1dc0d3285
zlkca <likzhang@gmail.com>
2018-09-14 09:46:14 -0400

    Return weixin echo string

--- server/boot/root.js
+++ server/boot/root.js
@@ -13,15 +13,16 @@ module.exports = function(server) {
     let timestamp = req.query.timestamp;
     let nonce = req.query.nonce;
     let signature = req.query.signature;
+    let echostr = req.query.echostr;
     let list = [token, timestamp, nonce].sort();
     let sha1 = crypto.createHash('sha1');
     let s = list.join('');
     let hash = sha1.update(s).digest('hex');
     console.log(hash);
     if (hash === signature) {
-      res.send(req.echostr);
+      res.send(echostr);
     } else {
-      res.send('hello');
+      res.send('');
     }
   });
 
