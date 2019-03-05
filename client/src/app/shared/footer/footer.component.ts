7bdb0b57f894fcdb02b87473507ca63bfb312ac2
zlkca <likzhang@gmail.com>
2019-03-05 00:15:04 -0500

    Add fixed menu icons to bottom

--- client/src/app/shared/footer/footer.component.ts
+++ client/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-    year:number = 2018;
-  constructor() { }
+  year: number = 2018;
+
+  constructor(private router: Router) {
+
+  }
 
   ngOnInit() {
   }
 
+  toHome() {
+    this.router.navigate(['home']);
+  }
+
+  toOrder() {
+    this.router.navigate(['orders']);
+  }
+
+  toCart() {
+    this.router.navigate(['orders']);
+  }
+
+  toAccount() {
+    this.router.navigate(['login']);
+  }
 }
