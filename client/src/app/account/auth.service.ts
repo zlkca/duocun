7005c04f345fed22d91a4b51beb8e2cc291e982b
zlkca <likzhang@gmail.com>
2019-03-28 13:42:36 -0400

    change localstorage to cookie

--- client/src/app/account/auth.service.ts
+++ client/src/app/account/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import * as Cookies from 'js-cookie';
 
 @Injectable()
 export class AuthService {
 
   setAccessToken(token: string) {
-    localStorage.setItem('token', token);
+    // localStorage.setItem('token', token);
+    Cookies.set('token', token);
   }
 
   getAccessToken(): string {
-    return localStorage.getItem('token');
+    // return localStorage.getItem('token');
+    return Cookies.get('token');
   }
 }
