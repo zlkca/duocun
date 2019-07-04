407ce8e504e51b05c32bdc5ae4a4af7593bc01dd
zlkca <likzhang@gmail.com>
2019-07-04 07:26:12 -0400

    update angular to 8

--- client/src/app/product/product.service.ts
+++ client/src/app/product/product.service.ts
@@ -1,21 +1,8 @@
-
-import { throwError as observableThrowError, Observable } from 'rxjs';
-import { fromPromise } from 'rxjs/observable/fromPromise';
-import { catchError, map, mergeMap } from 'rxjs/operators';
-
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
-import { environment } from '../../environments/environment';
-// import { ProductApi, LoopBackFilter, CategoryApi, Picture, PictureApi, LoopBackConfig } from '../lb-sdk';
-import { Product, Category } from './product.model';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { identifierModuleUrl } from '@angular/compiler';
 import { EntityService } from '../entity.service';
 import { AuthService } from '../account/auth.service';
-
-const APP = environment.APP;
-const API_URL = environment.API_URL;
 
 @Injectable({
   providedIn: 'root'
