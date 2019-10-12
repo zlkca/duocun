be76689771187da40082c2f73ed70e24c68e0e88
zlkca <likzhang@gmail.com>
2019-10-11 21:13:36 -0400

    before update mall schedule id

--- server/models/mall-schedule.ts
+++ server/models/mall-schedule.ts
@@ -8,39 +8,36 @@ export class MallSchedule extends Model{
 		super(dbo, 'mall_schedules');
   }
 
-  
+  // list(req: Request, res: Response) {
+  //   let query = null;
+  //   if (req.headers && req.headers.filter && typeof req.headers.filter === 'string') {
+  //     query = (req.headers && req.headers.filter) ? JSON.parse(req.headers.filter) : null;
+  //   }
 
+  //   let q = query;
+  //   if (q) {
+  //     if (q.where) {
+  //       q = query.where;
+  //     }
+  //   } else {
+  //     q = {};
+  //   }
 
-  list(req: Request, res: Response) {
-    let query = null;
-    if (req.headers && req.headers.filter && typeof req.headers.filter === 'string') {
-      query = (req.headers && req.headers.filter) ? JSON.parse(req.headers.filter) : null;
-    }
+  //   if(q && q.mallId){
+  //     q.mallId = new ObjectID(q.mallId);
+  //   }
 
-    let q = query;
-    if (q) {
-      if (q.where) {
-        q = query.where;
-      }
-    } else {
-      q = {};
-    }
-
-    if(q && q.mallId){
-      q.mallId = new ObjectID(q.mallId);
-    }
-
-    const params = [
-      {$lookup: {from: 'malls', localField: 'mallId', foreignField: '_id', as: 'mall'}},
-      {$unwind: '$mall'}
-    ];
-    this.join(params, q).then((rs: any) => {
-      res.setHeader('Content-Type', 'application/json');
-      if (rs) {
-        res.send(JSON.stringify(rs, null, 3));
-      } else {
-        res.send(JSON.stringify(null, null, 3))
-      }
-    });
-  }
+  //   const params = [
+  //     {$lookup: {from: 'malls', localField: 'mallId', foreignField: '_id', as: 'mall'}},
+  //     {$unwind: '$mall'}
+  //   ];
+  //   this.join(params, q).then((rs: any) => {
+  //     res.setHeader('Content-Type', 'application/json');
+  //     if (rs) {
+  //       res.send(JSON.stringify(rs, null, 3));
+  //     } else {
+  //       res.send(JSON.stringify(null, null, 3))
+  //     }
+  //   });
+  // }
 }
