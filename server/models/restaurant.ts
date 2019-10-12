a9ee6447c6e920d773b7ee290496c3d2c453f11e
zlkca <likzhang@gmail.com>
2019-10-11 21:34:04 -0400

    After update products merchants contacts and orders id, update find function

--- server/models/restaurant.ts
+++ server/models/restaurant.ts
@@ -44,44 +44,44 @@ export class Restaurant extends Model {
     this.range = new Range(dbo);
   }
 
-  // list(req: Request, res: Response) {
-  //   let query = null;
-  //   if (req.headers && req.headers.filter && typeof req.headers.filter === 'string') {
-  //     query = (req.headers && req.headers.filter) ? JSON.parse(req.headers.filter) : null;
-  //   }
+  list(req: Request, res: Response) {
+    let query = null;
+    if (req.headers && req.headers.filter && typeof req.headers.filter === 'string') {
+      query = (req.headers && req.headers.filter) ? JSON.parse(req.headers.filter) : null;
+    }
 
-  //   let q = query;
-  //   if (q) {
-  //     if (q.where) {
-  //       q = query.where;
-  //     }
-  //   } else {
-  //     q = {};
-  //   }
+    let q = query;
+    if (q) {
+      if (q.where) {
+        q = query.where;
+      }
+    } else {
+      q = {};
+    }
 
-  //   if(q && q._id){
-  //     q._id = new ObjectID(q._id);
-  //   }
+    if(q && q._id){
+      q._id = new ObjectID(q._id);
+    }
 
-  //   if(q && q.mallId){
-  //     q.mallId = new ObjectID(q.mallId);
-  //   }
+    if(q && q.mallId){
+      q.mallId = new ObjectID(q.mallId);
+    }
 
-  //   const params = [
-  //     {$lookup: {from: 'malls', localField: 'mallId', foreignField: '_id', as: 'mall'}},
-  //     {$unwind: '$mall'}
-  //     // {from: 'restaurants', localField: 'merchantId', foreignField: '_id', as: 'merchant'}
-  //   ];
+    const params = [
+      {$lookup: {from: 'malls', localField: 'mallId', foreignField: '_id', as: 'mall'}},
+      {$unwind: '$mall'}
+      // {from: 'restaurants', localField: 'merchantId', foreignField: '_id', as: 'merchant'}
+    ];
 
-  //   this.join(params, q).then((rs: any) => {
-  //     res.setHeader('Content-Type', 'application/json');
-  //     if (rs) {
-  //       res.send(JSON.stringify(rs, null, 3));
-  //     } else {
-  //       res.send(JSON.stringify(null, null, 3))
-  //     }
-  //   });
-  // }
+    this.join(params, q).then((rs: any) => {
+      res.setHeader('Content-Type', 'application/json');
+      if (rs) {
+        res.send(JSON.stringify(rs, null, 3));
+      } else {
+        res.send(JSON.stringify(null, null, 3))
+      }
+    });
+  }
   
   loadByDeliveryInfo(origin: ILocation, delivered: string): Promise<any> {
 
