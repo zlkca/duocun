5b974b54c8717a572e6f84e10737b8971dfab4d0
zlkca <likzhang@gmail.com>
2019-10-11 21:08:46 -0400

    before update product id

--- server/models/product.ts
+++ server/models/product.ts
@@ -21,44 +21,44 @@ export class Product extends Model {
     }
   }
 
-  list(req: Request, res: Response) {
-    let query = null;
-    if (req.headers && req.headers.filter && typeof req.headers.filter === 'string') {
-      query = (req.headers && req.headers.filter) ? JSON.parse(req.headers.filter) : null;
-    }
+  // list(req: Request, res: Response) {
+  //   let query = null;
+  //   if (req.headers && req.headers.filter && typeof req.headers.filter === 'string') {
+  //     query = (req.headers && req.headers.filter) ? JSON.parse(req.headers.filter) : null;
+  //   }
 
-    let q = query;
-    if (q) {
-      if (q.where) {
-        q = query.where;
-      }
-    } else {
-      q = {};
-    }
+  //   let q = query;
+  //   if (q) {
+  //     if (q.where) {
+  //       q = query.where;
+  //     }
+  //   } else {
+  //     q = {};
+  //   }
 
-    if(q && q.merchantId){
-      q.merchantId = new ObjectID(q.merchantId);
-    }
+  //   if(q && q.merchantId){
+  //     q.merchantId = new ObjectID(q.merchantId);
+  //   }
 
-    if(q && q.categoryId){
-      q.categoryId = new ObjectID(q.categoryId);
-    }
+  //   if(q && q.categoryId){
+  //     q.categoryId = new ObjectID(q.categoryId);
+  //   }
 
-    const params = [
-      {$lookup:{from: 'categories', localField: 'categoryId', foreignField: '_id', as: 'category'}},
-      {$unwind:'$category'},
-      {$lookup:{from: 'restaurants', localField: 'merchantId', foreignField: '_id', as: 'merchant'}},
-      {$unwind:'$merchant'}
-    ];
-    this.join(params, q).then((rs: any) => {
-      res.setHeader('Content-Type', 'application/json');
-      if (rs) {
-        res.send(JSON.stringify(rs, null, 3));
-      } else {
-        res.send(JSON.stringify(null, null, 3))
-      }
-    });
-  }
+  //   const params = [
+  //     {$lookup:{from: 'categories', localField: 'categoryId', foreignField: '_id', as: 'category'}},
+  //     {$unwind:'$category'},
+  //     {$lookup:{from: 'restaurants', localField: 'merchantId', foreignField: '_id', as: 'merchant'}},
+  //     {$unwind:'$merchant'}
+  //   ];
+  //   this.join(params, q).then((rs: any) => {
+  //     res.setHeader('Content-Type', 'application/json');
+  //     if (rs) {
+  //       res.send(JSON.stringify(rs, null, 3));
+  //     } else {
+  //       res.send(JSON.stringify(null, null, 3))
+  //     }
+  //   });
+  // }
 
   // load(req: Request, res: Response) {
   //   let query = null;
