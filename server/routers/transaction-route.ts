1532458100d1814d9283de3b49e40ac9d2816425
zlkca <likzhang@gmail.com>
2019-10-20 09:23:10 -0400

    Add quickFind to transaction

--- server/routers/transaction-route.ts
+++ server/routers/transaction-route.ts
@@ -6,6 +6,7 @@ export function TransactionRouter(db: DB){
   const router = express.Router();
   const controller = new Transaction(db);
 
+  router.get('/qFind', (req, res) => { controller.quickFind(req, res); });
   router.get('/', (req, res) => { controller.list(req, res); });
   router.get('/:id', (req, res) => { controller.get(req, res); });
   router.post('/', (req, res) => { controller.create(req, res); });
